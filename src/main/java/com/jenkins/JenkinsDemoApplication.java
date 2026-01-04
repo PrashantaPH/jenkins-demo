c4ce2f9392e4005c85f09a3ca01d8ec874dc653e
PrashantaPH <105696439+PrashantaPH@users.noreply.github.com>
2026-01-04 17:19:22 +0530

    Update JenkinsDemoApplication.java

--- src/main/java/com/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -3,13 +3,22 @@ package com.jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jenkins"})
+@RestController // Add this
+@RequestMapping("/api") // Add this
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
+	@GetMapping("/message")
+	public String message() {
+		return "Direct mapping test working!";
+	}
 }
