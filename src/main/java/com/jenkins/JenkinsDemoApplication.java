5f44c1b435edfb46b27ee61d8ff3d9f72fe1ae40
PrashantaPH <105696439+PrashantaPH@users.noreply.github.com>
2026-01-04 17:22:02 +0530

    Update JenkinsDemoApplication.java

--- src/main/java/com/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -2,15 +2,13 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.jenkins"})
-@RestController // Add this
-@RequestMapping("/api") // Add this
+@RestController
+@RequestMapping("/api")
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
